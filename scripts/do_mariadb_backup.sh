41ced3296e291a7c9e0e211bda59c85e14bc814f
Tomaz <tomazkunst@gmail.com>
2022-06-03 14:53:05 +0200

    mautic backup

---
+++ scripts/do_mariadb_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set backup file name
+export DUMP_FILE=${DATABASE_NAME}_db_`date +%Y%m%d_%H%M%S`.sql
+
+# dump the database
+MARIADB_PASSWORD=$DATABASE_PASSWORD \
+    mysqldump -A -u $DATABASE_USER > $DUMP_FILE
+
+# zip the database
+bzip2 $DUMP_FILE
+
+# encrypt the database
+mcrypt ${DUMP_FILE}.bz2 -k $DB_BACKUP_PASSWORD
+
+# upload the database to s3
+aws s3 cp ${DUMP_FILE}.bz2.nc $S3_BACKUP_PATH \
+    --endpoint-url=https://s3.fr-par.scw.cloud \
+    --region=fr-par
